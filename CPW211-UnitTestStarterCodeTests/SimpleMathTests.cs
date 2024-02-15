c10fcc7a41b6dab9c372a0e975d1bef60508b465
Jennifer Peterson <jennpeterson986@gmail.com>
2024-02-14 21:40:19 -0800

    Added test logic

--- CPW211-UnitTestStarterCodeTests/SimpleMathTests.cs
+++ CPW211-UnitTestStarterCodeTests/SimpleMathTests.cs
@@ -18,27 +18,60 @@ namespace CPW211_UnitTestStarterCode.Tests
         [DataRow(0, -0)]
         public void Add_TwoNumbers_ReturnsSum(double num1, double num2)
         {
-            // Use the DataRow values to test the Add method
-            Assert.Fail();
+            double expectedReturn = num1 + num2;
+            double actualReturn = SimpleMath.Add(num1, num2);
+
+            Assert.AreEqual(expectedReturn, actualReturn);
         }
 
-        [TestMethod]
-        public void Multiply_TwoNumbers_ReturnsProduct()
+        // TODO: Test subtract method with two valid numbers
+        [TestMethod()]
+        [DataRow(5, 10)]
+        [DataRow(0, 100)]
+        [DataRow(-1, -10)]
+        [DataRow(0, -0)]
+        public void Subtract_TwoNumbers_ReturnsDifference(double num1, double num2)
         {
-            // Use a few pairs of values to test the Multiply method
-            Assert.Fail();
+            double expectedReturn = num1 - num2;
+            double actualReturn = SimpleMath.Subtract(num1, num2);
+
+            Assert.AreEqual(expectedReturn, actualReturn);
         }
 
         [TestMethod]
-        public void Divide_DenominatorZero_ThrowsArgumentException()
+        [DataRow(5, 10)]
+        [DataRow(0, 100)]
+        [DataRow(-1, -10)]
+        [DataRow(0, -0)]
+        public void Multiply_TwoNumbers_ReturnsProduct(double num1, double num2)
+        {
+
+            double expectedReturn = num1 * num2;
+            double actualReturn = SimpleMath.Multiply(num1, num2);
+
+            Assert.AreEqual(expectedReturn, actualReturn);
+        }
+
+        [TestMethod]
+        [DataRow(5, 0)]
+        public void Divide_DenominatorZero_ThrowsArgumentException(double num1, double num2)
         {
             // Divide by zero should throw an argument exception with a message
             // "Denominator cannot be zero"
-            Assert.Fail();
+            Assert.ThrowsException<DivideByZeroException>(() => SimpleMath.Divide(num1, num2));
         }
 
         // TODO: Test Divide method with two valid numbers
+        [TestMethod()]
+        [DataRow(20, 4)]
+        [DataRow(1, 3)]
+        [DataRow(8, -8)]
+        public void Divide_TwoNumbers_ReturnsQuotient(double num1, double num2)
+        {
+            double expectedReturn = num1 / num2;
+            double actualReturn = SimpleMath.Divide(num1, num2);
 
-        // TODO: Test subtract method with two valid numbers
+            Assert.AreEqual(expectedReturn, actualReturn);
+        }
     }
 }
